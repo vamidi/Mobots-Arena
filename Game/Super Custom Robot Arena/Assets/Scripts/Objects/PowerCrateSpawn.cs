18a0b1c5cdcba7c22a75707f41cf7688feae2f11
LAPTOP-5BEKO9SE\johan <johanst1409@gmail.com>
2017-01-19 12:30:08 +0100

    Fixed issues with the Power Cube spawners

--- Game/Super Custom Robot Arena/Assets/Scripts/Objects/PowerCrateSpawn.cs
+++ Game/Super Custom Robot Arena/Assets/Scripts/Objects/PowerCrateSpawn.cs
@@ -3,35 +3,60 @@ using System.Collections;
 
 public class PowerCrateSpawn : MonoBehaviour {
     public GameObject PowerCrate;
-    private GameObject[] spawnPoints;
-    public float interval;
-    private float internInterval;
+    public float firstInterval;
+    public float minInterval;
+    public float maxInterval;
 
-    public int x;
-	// Use this for initialization
+    private GameObject[] spawnPoints;
+    private float internInterval;
+    private int pointCount;
+
 	void Start () {
+        //seek out all gameobjects tagged with "PowerCrateSpawn", and push them to the array.
         spawnPoints = GameObject.FindGameObjectsWithTag("PowerCrateSpawn");
-        SpawnPowerUp();
-        Debug.Log(spawnPoints.Length);
+        //set the first interval
+        internInterval = firstInterval;
     }
 	
-	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+        //Countdown from the intervaltime to zero (substract time between fixed updates from time left)
         internInterval -= Time.deltaTime;
         //Debug.Log(internInterval);
         if (internInterval <= 0)
         {
-            SpawnPowerUp();
-            internInterval = interval;
+            //Reset pointCount
+            pointCount = 0;
+            //foreach to check if there are free spawnpoints
+            foreach (GameObject point in spawnPoints)
+            {
+                //If the spawnpoint has a child add 1 to the count of occupied spawnpoints
+                if(point.transform.childCount != 0)
+                    pointCount++;
+            }
+            //Debug.Log(pointCount);
+            //If there is a free spawnpoint continue
+            if (pointCount != spawnPoints.Length)
+            {
+                //spawn a power-up
+                SpawnPowerUp();
+
+                //reset timer, value between the minimal interval and the maximal interval.
+                internInterval = Random.Range(minInterval, maxInterval);
+                //Debug.Log(internInterval);
+            }
         }
     }
 
     void SpawnPowerUp()
     {
-        x = Random.Range(0, spawnPoints.Length);
+        // generate a random integer between  and the amount of spawnpoints
+        int x = Random.Range(0, spawnPoints.Length);
+        //Get a spawnpoint check if it doesn't have a child
         if(spawnPoints[x].transform.childCount == 0)
         {
+            //spawn a power-up cube and place it on the spawnpoint.
             GameObject newCube = Instantiate(PowerCrate, spawnPoints[x].transform.position, Quaternion.identity);
+            //make the power-up cube a child of the spawnpoint.
             newCube.transform.parent = spawnPoints[x].transform;
         }
     }
--- Game/Super Custom Robot Arena/Assets/Scripts/Objects/PowerUpCube.cs
+++ Game/Super Custom Robot Arena/Assets/Scripts/Objects/PowerUpCube.cs
@@ -4,16 +4,17 @@ using System.Collections;
 public class PowerUpCube : MonoBehaviour {
     private float amplitude = 0.2f;
     private float y0;
-    public GameObject hitParticle;
-
-    public string[] powerUps = {"Health", "Shield", "Special", "Boost"};
     private int x;
+
+    public GameObject hitParticle;
+    public string[] powerUps = {"Health", "Shield", "Special", "Boost"};
     
 
 	// Use this for initialization
 	void Start () {
         transform.position = new Vector3(transform.position.x, 2f, transform.position.z);
         y0 = transform.position.y;
+        x = Random.Range(0, powerUps.Length);
     }
 
     void OnTriggerEnter(Collider col)
@@ -21,7 +22,6 @@ public class PowerUpCube : MonoBehaviour {
         if (col.tag == "Head" || col.tag == "Left" || col.tag == "Right" || col.tag == "Car")
         {
             Destroy(this.gameObject);
-            x = Random.Range(0, powerUps.Length);
             //Debug.Log(col.tag);
             GameObject particle = (GameObject)Instantiate(hitParticle, this.transform.position, Quaternion.identity);
             Destroy(particle, 1.2f);
@@ -51,21 +51,25 @@ public class PowerUpCube : MonoBehaviour {
         transform.position = new Vector3(transform.position.x, y0 + amplitude * Mathf.Sin(5 * Time.time),transform.position.z);
     }
 
+    //TODO: function for the "Health" pick-up [effect, set to UI, etc]
     void Health()
     {
         Debug.Log("Health pickup");
     }
 
+    //TODO: function for the "Shield" pick-up [effect, set to UI, etc]
     void Shield()
     {
         Debug.Log("Shield pickup");
     }
 
+    //TODO: function for te "Special weapon" pick-up [effect, which special weapon, set to UI, etc]
     void Special()
     {
         Debug.Log("Special pickup");
     }
 
+    //TODO: function for te "Speed boost" pick-up [effect, set to UI, etc]
     void Boost()
     {
         Debug.Log("Speed Boost pickup");
