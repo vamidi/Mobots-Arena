4a4376eb27f1318f932ffee1ad4bfcf0423319ec
Valencio Hoffman <v.hoffman@idinteractive.nl>
2017-05-28 16:21:37 +0200

    MOB-027 New project added with menu scene. added animations, button listeners, triggers and few scripts

---
+++ Game/Mobots_menu/Assets/Scripts/UI/Menu/MenuController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace Mobots.UI {
+
+	public delegate void OnEnterState(int i);
+	public delegate void OnExitState(int i);
+	public class MenuController : MonoBehaviour {
+
+		[Header("Panels")] 
+		public string[] mPageNames;
+		public GameObject[] mPanels;
+		public int mCurrentPageName = 0;
+		public int mPreviousPage = 0;
+		
+		[Header("Robot Settings")] 
+		public GameObject mRobot;
+		
+		[Header("States")]
+		public OnEnterState mEnterState;
+		public OnExitState mExitState;
+		
+		public void QuitGame() {
+			Debug.Log("You have quite the game");
+		}
+		
+		// Use this for initialization
+		void Start() {
+			for (int i = 0; i < mPanels.Length; i++) {
+				if (i != 0) {
+					mPanels[i].SetActive(false);
+				}
+			}
+			mEnterState = OnEnterPanel;
+			mExitState = OnExitPanel;
+			if (mRobot) {
+				mRobot.SetActive(false);
+			}
+		}
+
+		// Update is called once per frame
+		void Update() { }
+
+		private void OnEnterPanel(int index) {
+			if (mPanels[index].GetComponent<Animator>()) {
+				mPanels[index].GetComponent<Animator>().SetTrigger("Open");
+			}
+		}
+		
+		private void OnExitPanel(int index) {
+			if (mPanels[index].GetComponent<Animator>()) {
+				mPanels[index].GetComponent<Animator>().SetTrigger("Close");
+			}
+		}
+
+		private void RevealPagePanel(int index, bool previous = false) {
+			// Exit state of the previous page
+			mExitState?.Invoke(mPreviousPage);
+
+			if (previous) {
+//				mPanels[mCurrentPageName].SetActive(false);
+//				mPanels[index].SetActive(true);
+				mCurrentPageName = index;
+			} else {
+//				mPanels[mPreviousPage].SetActive(false);
+//				mPanels[index].SetActive(true);
+			}
+			// Enter state of the current page
+			mEnterState?.Invoke(index);
+		}
+		
+		private void SetPreviousPage () {
+			for(int i = 0; i < mPageNames.Length; i++) {
+				if(mPreviousPage == i) {
+					RevealPagePanel(i, true);
+				}
+			}
+		}
+			
+		private void SetNextPage (string pageCode) {
+			for(int i = 0; i < mPageNames.Length; i++) {
+				if(pageCode == mPageNames[i]) {
+					mPreviousPage = mCurrentPageName;
+					mCurrentPageName = i;
+					RevealPagePanel(i);
+				}
+			}
+		}
+	}
+}
---
+++ Game/Mobots_menu/Assets/Scripts/Utils/SimpleRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleRotation : MonoBehaviour {
+
+	public float mSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// ...also rotate around the World's Y axis
+		transform.Rotate(Vector3.up * Time.deltaTime * mSpeed, Space.World);
+	}
+}
