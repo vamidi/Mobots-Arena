a6e5200bf9163939ecbde8fb44fa4323f91f784c
Valencio Hoffman <v.hoffman@idinteractive.nl>
2017-05-28 23:18:04 +0200

    MOB-027 Menu structure made with layout groups

--- Game/Mobots_menu/Assets/Scripts/UI/Menu/MenuController.cs
+++ Game/Mobots_menu/Assets/Scripts/UI/Menu/MenuController.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
 
 namespace Mobots.UI {
@@ -12,31 +11,28 @@ namespace Mobots.UI {
 		[Header("Panels")] 
 		public string[] mPageNames;
 		public GameObject[] mPanels;
-		public int mCurrentPageName = 0;
-		public int mPreviousPage = 0;
-		
+		public int mCurrentPageName;
+		public int mPreviousPage;
+
 		[Header("Robot Settings")] 
 		public GameObject mRobot;
-		
-		[Header("States")]
+
+		[Header("States")] 
 		public OnEnterState mEnterState;
 		public OnExitState mExitState;
-		
+
 		public void QuitGame() {
 			Debug.Log("You have quite the game");
 		}
-		
+
 		// Use this for initialization
 		void Start() {
-			for (int i = 0; i < mPanels.Length; i++) {
-				if (i != 0) {
-					mPanels[i].SetActive(false);
-				}
-			}
 			mEnterState = OnEnterPanel;
 			mExitState = OnExitPanel;
-			if (mRobot) {
-				mRobot.SetActive(false);
+			for (int i = 0; i < mPanels.Length; i++) {
+				if (i != 0) {
+					mPanels[i]?.GetComponent<Animator>()?.SetTrigger("Close");
+				}
 			}
 		}
 
@@ -44,44 +40,31 @@ namespace Mobots.UI {
 		void Update() { }
 
 		private void OnEnterPanel(int index) {
-			if (mPanels[index].GetComponent<Animator>()) {
-				mPanels[index].GetComponent<Animator>().SetTrigger("Open");
-			}
-		}
-		
-		private void OnExitPanel(int index) {
-			if (mPanels[index].GetComponent<Animator>()) {
-				mPanels[index].GetComponent<Animator>().SetTrigger("Close");
-			}
+			mPanels[index]?.GetComponent<Animator>()?.SetTrigger("Open");
+			mPanels[index]?.GetComponent<UIPanel>()?.OnEnterState();
 		}
 
-		private void RevealPagePanel(int index, bool previous = false) {
+		private void OnExitPanel(int index) {
+			mPanels[index]?.GetComponent<Animator>()?.SetTrigger("Close");
+			mPanels[index]?.GetComponent<UIPanel>()?.OnExitState();
+		}
+
+		private void RevealPagePanel(int index) {
 			// Exit state of the previous page
 			mExitState?.Invoke(mPreviousPage);
-
-			if (previous) {
-//				mPanels[mCurrentPageName].SetActive(false);
-//				mPanels[index].SetActive(true);
-				mCurrentPageName = index;
-			} else {
-//				mPanels[mPreviousPage].SetActive(false);
-//				mPanels[index].SetActive(true);
-			}
 			// Enter state of the current page
 			mEnterState?.Invoke(index);
 		}
-		
-		private void SetPreviousPage () {
-			for(int i = 0; i < mPageNames.Length; i++) {
-				if(mPreviousPage == i) {
-					RevealPagePanel(i, true);
-				}
-			}
+
+		private void SetPreviousPage() {
+			mExitState?.Invoke(mCurrentPageName);
+			mEnterState?.Invoke(mPreviousPage);
+			mCurrentPageName = mPreviousPage;
 		}
-			
-		private void SetNextPage (string pageCode) {
-			for(int i = 0; i < mPageNames.Length; i++) {
-				if(pageCode == mPageNames[i]) {
+
+		private void SetNextPage(string pageCode) {
+			for (int i = 0; i < mPageNames.Length; i++) {
+				if (pageCode == mPageNames[i]) {
 					mPreviousPage = mCurrentPageName;
 					mCurrentPageName = i;
 					RevealPagePanel(i);
---
+++ Game/Mobots_menu/Assets/Scripts/UI/Menu/UIPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mobots.UI {
+	public class UIPanel : MonoBehaviour {
+		public GameObject mGameObject;
+		public Vector3 mTransform;
+
+		public void Start() {
+			mTransform = mGameObject.GetComponent<Transform>().eulerAngles;
+		}
+		
+		public void OnEnterState() {
+			if (mGameObject) {
+				mGameObject?.SetActive(true);
+			}
+		}
+		
+		public void OnExitState() {
+			if (mGameObject) {
+				mGameObject.transform.eulerAngles = mTransform;
+				mGameObject?.SetActive(false);
+			}
+		}
+	}
+}
